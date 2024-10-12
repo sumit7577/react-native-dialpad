f276af047f6d57dae17fb853408f142821df3e03
sumit7577 <asmitkumarr082@gmail.com>
2024-10-12 09:07:03 -0700

    fixed popup set default

--- android/src/main/java/com/reactdialpad/DialPadHelper.kt
+++ android/src/main/java/com/reactdialpad/DialPadHelper.kt
@@ -1,13 +1,18 @@
 package com.reactdialpad
 
+import android.app.Activity
+import android.app.role.RoleManager
+import android.content.Context.ROLE_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.telecom.TelecomManager
 import android.telecom.TelecomManager.ACTION_CHANGE_DEFAULT_DIALER
 import android.telecom.TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat.startActivity
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.getSystemService
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -39,6 +44,19 @@ class DialPadHelper(reactContext: ReactApplicationContext) : ReactContextBaseJav
     } else {
       Toast.makeText(reactApplicationContext, "TelecomManager not available", Toast.LENGTH_SHORT).show()
     }
+  }
+
+  @ReactMethod
+  @RequiresApi(Build.VERSION_CODES.Q)
+  fun requestRole() {
+    // Get RoleManager system service
+    val activity = currentActivity;
+    val roleManager = reactApplicationContext.getSystemService(RoleManager::class.java)
+
+    // Create the request role intent
+    val intent = roleManager?.createRequestRoleIntent(RoleManager.ROLE_DIALER)
+    activity?.startActivityForResult(intent,1)
+    Toast.makeText(reactApplicationContext,activity!!.packageName,Toast.LENGTH_LONG).show()
   }
 
   companion object{
